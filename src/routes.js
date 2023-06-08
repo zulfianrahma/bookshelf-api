9ac5fe4622f0f73ef8c773cf9af5021fd8902bb6
Zulfian Rahmadiansyah <zulfian2018@mail.ugm.ac.id>
2023-06-08 11:33:29 +0700

    update submission

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,36 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/books',
-    handler: () => {},
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
   },
 ];
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 8080,
+    port: 9000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
